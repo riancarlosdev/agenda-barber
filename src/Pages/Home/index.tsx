dd0b8a83a7b3a79b8e6e59726c2ff5194550b4d5
riancarlosdev <riancontatoprofissional@gmail.com>
2022-06-28 21:32:48 -0300

    add step what's name

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,10 +1,13 @@
 import { MaxWidthComponent } from '@components/Global/MaxWidth';
 import { HeaderComponent } from '@components/Header';
 import { SectionComponent } from '@components/Section';
+import { useState } from 'react';
 import { StepSelectBarbersComponent } from './Steps/Barbers';
-import { WapperContainer } from './styles';
+import { WapperContainer, WapperInputName, Input, WapperTotalLength } from './styles';
 
 export const HomePage: React.FC = (): JSX.Element => {
+  const [lengthInputName, setLengthInputName] = useState<number>(0);
+
   return (
     <MaxWidthComponent>
       <WapperContainer>
@@ -12,6 +15,13 @@ export const HomePage: React.FC = (): JSX.Element => {
 
         <SectionComponent title='Escolha a Barbearia:'>
           <StepSelectBarbersComponent />
+        </SectionComponent>
+
+        <SectionComponent title='Digite seu nome:'>
+          <WapperInputName>
+            <Input maxLength={23} onChange={(e) => setLengthInputName(e.target.value.length)} />
+            <WapperTotalLength status={lengthInputName}>{lengthInputName}/23</WapperTotalLength>
+          </WapperInputName>
         </SectionComponent>
       </WapperContainer>
     </MaxWidthComponent>
--- src/Pages/Home/styles.tsx
+++ src/Pages/Home/styles.tsx
@@ -1,3 +1,65 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const WapperContainer = styled.div``;
+
+// STEP YOUR NAME
+export const WapperInputName = styled.label`
+  max-width: 350px;
+  width: 100%;
+  position: relative;
+`;
+
+export const Input = styled.input`
+  outline: none;
+  border: none;
+
+  max-width: 350px;
+  width: 100%;
+
+  height: 35px;
+  padding-left: 10px;
+  border: 1px solid #cecece;
+  box-shadow: 1px 2px 3px #e3e3e3;
+
+  font-size: 17px;
+`;
+
+export const WapperTotalLength = styled.div<{ status?: number }>`
+  position: absolute;
+  width: 55px;
+  height: 35px;
+
+  letter-spacing: 1px;
+  font-weight: 600;
+  font-size: 14px;
+
+  ${({ status }) => {
+    if (status && status > 20) {
+      return css`
+        background: #ff0f0f;
+        color: #fff;
+      `;
+    }
+    if (status && status >= 11) {
+      return css`
+        background: #cb9200;
+      `;
+    }
+    if ((status && status < 11) || status === 0) {
+      return css`
+        background: #7df521;
+      `;
+    }
+  }}
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  top: -8px;
+  right: 0;
+
+  border-top: 1px solid #cecece;
+  border-bottom: 1px solid #cecece;
+  border-right: 1px solid #cecece;
+`;
